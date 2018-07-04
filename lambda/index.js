70ebd0af3c638610423967a516797f02e5fec52a
Shane Hughes <shanehughes314@gmail.com>
2018-07-04 08:40:52 -0500

    initial setup with simpledb

--- lambda/index.js
+++ lambda/index.js
@@ -1,22 +1,48 @@
+const aws = require('aws-sdk');
+
 exports.handler = (event, context, cb) => {
 	let input;
 	const res = {};
 	try {
 		input = JSON.parse(event.body);
-		res.statusCode = 200;
-		res.body = { result: 'OK' };
-		console.log(input);
+		['lb', 'oz', 'in', 'sex', 'c', 'b', 'date'].forEach((key) => {
+			if (!input.hasOwnProperty(key)) {
+				throw new Error(`Missing required value ${key}`);
+			}
+		});
+		submitToSDB(input)
+			.then(() => {
+				res.statusCode = 204;
+				cb(null, res);
+			});
 	} catch (err) {
 		if (err.name === 'SyntaxError') {
 			res.statusCode = 400;
-			res.body = { error: 'Non-JSON body' };
+			res.body = {
+				error: 'Non-JSON body',
+				input: event.body
+			};
 		} else {
 			console.log(err);
 			res.statusCode = 500;
 		}
+		if (res.body) {
+			res.body = JSON.stringify(res.body);
+		}
+		cb(null, res);
 	}
-	if (res.body) {
-		res.body = JSON.stringify(res.body);
-	}
-	cb(null, res);
 };
+
+function submitToSDB(data) {
+	return new Promise((resolve) => {
+		const cred = new aws.Credentials({
+			accessKeyId: process.env.KEY_ID,
+			secretAccessKey: process.env.SECRET_KEY
+		});
+		const sdb = new aws.SimpleDB(new aws.Config({
+			credentials: cred,
+			region: 'us-east-1'
+		}));
+		resolve();
+	});
+}
